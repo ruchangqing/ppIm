9e5a5c2e8b67707b34f134e5f00704a97704435c
Panco <1129443982@qq.com>
2021-04-23 17:15:32 +0800

    更新sql

--- sql/database.sql
+++ sql/database.sql
@@ -11,96 +11,11 @@
  Target Server Version : 50728
  File Encoding         : 65001
 
- Date: 23/04/2021 10:33:37
+ Date: 23/04/2021 17:15:10
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for chat_user
--- ----------------------------
-DROP TABLE IF EXISTS `chat_user`;
-CREATE TABLE `chat_user`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `send_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发送用户id',
-  `recv_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '接收用户id',
-  `message_type` tinyint(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT '消息类型，1文字',
-  `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '消息内容',
-  `created_at` int(10) NULL DEFAULT NULL COMMENT '发送时间',
-  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态，0未读，1已读，-1撤回',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '私聊聊天记录表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for friend_add
--- ----------------------------
-DROP TABLE IF EXISTS `friend_add`;
-CREATE TABLE `friend_add`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
-  `f_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '好友uid',
-  `channel` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '加好友途径，搜索、附近的人',
-  `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '添加原因',
-  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态，1同意，0等待，-1拒绝',
-  `request_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加好友请求时间戳',
-  `pass_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '通过好友时间戳',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '添加好友表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for friend_list
--- ----------------------------
-DROP TABLE IF EXISTS `friend_list`;
-CREATE TABLE `friend_list`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
-  `f_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '好友uid',
-  `channel` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '加好友途径，搜索、附近的人',
-  `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '添加原因',
-  `role` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1加的那一方，2被加的那一方',
-  `created_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间戳',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `unique`(`uid`, `f_uid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '好友表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for group
--- ----------------------------
-DROP TABLE IF EXISTS `group`;
-CREATE TABLE `group`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `o_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '群主id',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '群组名称',
-  `created_at` int(10) UNSIGNED NULL DEFAULT 0 COMMENT '创建时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '群组表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for group_join
--- ----------------------------
-DROP TABLE IF EXISTS `group_join`;
-CREATE TABLE `group_join`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `group_id` int(10) UNSIGNED NOT NULL DEFAULT 10,
-  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `join_at` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '群组申请表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for group_user
--- ----------------------------
-DROP TABLE IF EXISTS `group_user`;
-CREATE TABLE `group_user`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `group_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '群组id',
-  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
-  `join_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '进群时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `group_id`(`group_id`) USING BTREE,
-  INDEX `user_id`(`user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '群组用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
@@ -130,5 +45,91 @@ CREATE TABLE `user`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for group_user
+-- ----------------------------
+DROP TABLE IF EXISTS `group_user`;
+CREATE TABLE `group_user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `group_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '群组id',
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `join_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '进群时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `group_id`(`group_id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '群组用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for group_join
+-- ----------------------------
+DROP TABLE IF EXISTS `group_join`;
+CREATE TABLE `group_join`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `group_id` int(10) UNSIGNED NOT NULL DEFAULT 10,
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `join_at` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '群组申请表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for group
+-- ----------------------------
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE `group`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `o_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '群主id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '群组名称',
+  `created_at` int(10) UNSIGNED NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '群组表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for friend_list
+-- ----------------------------
+DROP TABLE IF EXISTS `friend_list`;
+CREATE TABLE `friend_list`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `f_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '好友uid',
+  `channel` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '加好友途径，搜索、附近的人',
+  `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '添加原因',
+  `role` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1加的那一方，2被加的那一方',
+  `created_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间戳',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `unique`(`uid`, `f_uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '好友表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for friend_add
+-- ----------------------------
+DROP TABLE IF EXISTS `friend_add`;
+CREATE TABLE `friend_add`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `f_uid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '好友uid',
+  `channel` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '加好友途径，搜索、附近的人',
+  `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '添加原因',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态，1同意，0等待，-1拒绝',
+  `request_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加好友请求时间戳',
+  `pass_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '通过好友时间戳',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '添加好友表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for chat_message
+-- ----------------------------
+DROP TABLE IF EXISTS `chat_message`;
+CREATE TABLE `chat_message`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `from_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '发送者id',
+  `to_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '接收者id',
+  `ope` tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '消息通道：0：好友消息，1：群消息',
+  `type` tinyint(3) UNSIGNED NOT NULL DEFAULT 1 COMMENT '消息类型：0 文本消息，1 图片，2 语音，3 视频，4 地理位置信息，6 文件，10 提示消息',
+  `body` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '消息内容',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0未读，1已读，-1已撤回',
+  `created_at` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '时间戳',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '消息记录' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
