6cb65ddd7bdb744a8ee52d5911956e62ed7b5886
Panco <1129443982@qq.com>
2021-04-27 14:43:39 +0800

    修改获取内网ip方法

--- utils/ip.go
+++ utils/ip.go
@@ -1,16 +1,18 @@
 package utils
 
+import "net"
+
 //获取本机内网IP
 func GetIntranetIp() string {
-	//addrs, _ := net.InterfaceAddrs()
-	//for _, addr := range addrs {
-	//	// 检查ip地址判断是否回环地址
-	//	if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-	//		if ipnet.IP.To4() != nil {
-	//			return ipnet.IP.String()
-	//		}
-	//
-	//	}
-	//}
-	return "127.0.0.1"
+	addrs, _ := net.InterfaceAddrs()
+	for _, addr := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+
+		}
+	}
+	//return "127.0.0.1"
 }
